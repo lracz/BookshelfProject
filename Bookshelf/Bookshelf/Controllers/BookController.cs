3089eb344c3fcfda9d8d175d24dfdcd35792d696
lracz <raczladislav088@gmail.com>
2023-06-19 10:57:09 +0200

    Finished project with swagger

---
+++ Bookshelf/Bookshelf/Controllers/BookController.cs
@@ -0,0 +1,61 @@
+﻿using Bookshelf.Models;
+using Bookshelf.Models.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Npgsql;
+
+namespace Bookshelf.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BookController : ControllerBase
+    {
+        private readonly BookRepository _repository;
+
+        
+        public BookController()
+        {
+            string connectionString = "Server=localhost;Port=5432;User Id=postgres;Password=1234;Database=bookshelf";
+
+            _repository = new BookRepository(connectionString);
+        }
+
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(_repository.GetAll());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var book = _repository.GetById(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            return Ok(book);
+        }
+
+        [HttpPost]
+        public IActionResult Create(Book book)
+        {
+            var bookId = _repository.Create(book);
+            return Ok(bookId);
+        }
+
+        [HttpPut]
+        public IActionResult Update(Book book)
+        {
+            _repository.Update(book);
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _repository.Delete(id);
+            return Ok();
+        }
+    }
+}
---
+++ Bookshelf/Bookshelf/Startup.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Npgsql;
+
+namespace Bookshelf
+{
+    public class Startup
+    {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddSwaggerGen();
+            // Add NpgsqlConnection as a singleton service
+            services.AddSingleton<NpgsqlConnection>(sp =>
+                new NpgsqlConnection(_configuration.GetConnectionString("DefaultConnection")));
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "bookshelf");
+            });
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
